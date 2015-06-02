72452e9f0a433a5b57f24306eaa349761ac7a7fd
Oleksii Novikov <hunter@nixsolutions.com>
2015-06-02 12:58:59 +0300

    Fixtures

--- app/pods/track-list/route.js
+++ app/pods/track-list/route.js
@@ -2,17 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model (params) {
-    //this.userSessionService.get('username');
     return this.store.find('track-list', params.track_list_id);
-    //var tl = null;
-    //return this.store.find('track-list', { raiting: 'positive' }).then((trackList) => {
-    //  tl = trackList;
-    //  return this.store.find('track', { trackList: trackList.id });
-    //}).then((tracks) => {
-    //  return tl;
-    //});
-    //var trackList = store.createRecord('track-list', {});
-    //trackList.save();
   }
 
 });
