0d3fed2e510d3514e5c065104ee38d0fc5bf2514
Oleksii Novikov <hunter@nixsolutions.com>
2015-06-03 17:32:48 +0300

    add-track-component

--- app/pods/components/sx-modal/component.js
+++ app/pods/components/sx-modal/component.js
@@ -1,14 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    title: null,
-    onClose: null,
+  title: null,
+  onClose: null,
 
-    //actions: {
-      closeMe () {
-        this.sendAction('onClose');
-      }
-    //}
+  actions: {
+    closeMe () {
+      this.sendAction('onClose');
+    }
   }
 });
