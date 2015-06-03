ffdd4b23ad2b52007e86971d78cf6f48845a1478
Oleksii Novikov <hunter@nixsolutions.com>
2015-06-03 12:52:23 +0300

    add track dialog url stubs

--- app/router.js
+++ app/router.js
@@ -6,10 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('track-list', {path: 'track-list/:track_list_id'}, function() {
-    this.resource('track', { path: ':track_id' });
-  });
+  this.route('track-list', { path: ':track_list_id'}, function() {
+    this.resource('track/new');
+    this.resource('track', { path: ':track_id' }, function() {
 
+    });
+  });
 });
 
 export default Router;
