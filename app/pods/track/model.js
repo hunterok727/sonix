97d06ef0895a1e9a8221167b6bb827af9d535c44
Oleksii Novikov <hunter@nixsolutions.com>
2015-06-03 12:29:12 +0300

    Track details page

--- app/pods/track/model.js
+++ app/pods/track/model.js
@@ -4,7 +4,11 @@ let Track = DS.Model.extend({
   artist: DS.attr('string'),
   title: DS.attr('string'),
 
-  trackList: DS.belongsTo('track-list')
+  trackList: DS.belongsTo('track-list'),
+
+  details: function () {
+    return this.get('title') + ' by ' + this.get('artist');
+  }.property('artist', 'title')
 });
 
 export default Track;
