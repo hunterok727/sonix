dac73f63f77529b0a68fbc8a6cf1aa37368faf82
Oleksii Novikov <hunter@nixsolutions.com>
2015-06-04 18:34:46 +0300

    remember active transition on login

--- app/initializers/session.js
+++ app/initializers/session.js
@@ -1,6 +1,20 @@
 export function initialize( container, application ) {
-   application.inject('route', 'session', 'service:session');
-   application.inject('controller:application', 'session', 'service:session');
+  application.inject('route', 'session', 'service:session');
+  application.inject('controller:application', 'session', 'service:session');
+
+  application.register('service:session-check', function beforeModel(transition) {
+    //this = route
+    var isLoggedIn = this.session.get('isLoggedIn');
+    if (!isLoggedIn) {
+      this.session.set('attemptedTransition', transition);
+      this.transitionTo('login');
+    }
+  }, { instantiate: false });
+
+  ['track', 'track/new'].forEach((route) => {
+    application.inject('route:' + route, 'beforeModel', 'service:session-check')
+  });
+
 }
 
 export default {
--- app/services/session.js
+++ app/services/session.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  attemptedTransition: null,
   username: null,
   isLoggedIn: Ember.computed.notEmpty('username')
 
