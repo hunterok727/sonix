43bc1df223cd9c4e5387fe254a0de9deebc61978
Oleksii Novikov <hunter@nixsolutions.com>
2015-06-09 17:09:40 +0300

    Pagination

--- app/pods/track-list/controller.js
+++ app/pods/track-list/controller.js
@@ -3,11 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['page', 'limit'],
   page: 1,
-  limit: 3,
+  limit: 3
 
-  actions: {
-    paging: function (params) {
-      console.log(params);
-    }
-  }
 });
